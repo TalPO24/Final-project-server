378a482b285ce2df2d184e31e2419b8c1664ee80
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-05-04 14:00:40 +0300

    Updated

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -17,14 +17,7 @@ const { createHash, cmpHash } = require("../../config/bcrypt");
 
 const { genToken, verifyToken } = require("../../config/jwt");
 
-//* This code is a route handler for a POST request to the "/register" endpoint using the Express.js router.
-//* When this route is hit, it first attempts to validate the request body against a validation schema using the validateRegisterSchema function.
-//* It then checks if a user already exists with the email provided in the request body using the findUserByEmail function.
-//* If a user already exists with that email, an error message is thrown.
-//* If not, the password provided in the request body is hashed using the createHash function.
-//* The newly hashed password is then added to the validated request body, and a new user is created using the createNewUser function.
-//* If no errors occur, a JSON object with a message of "user created" and a status code of 201 is sent in the response.
-//* If any errors occur during this process, a JSON object with the error message and a status code of 400 is sent in the response instead.
+
 router.post("/register", async(req, res) => {
     try {
         const validatedValue = await validateRegisterSchema(req.body);
@@ -46,15 +39,7 @@ router.post("/register", async(req, res) => {
     }
 });
 
-//* This code is a route handler for a POST request to the "/login" endpoint using the Express.js router.
-//* When this route is hit, it first attempts to validate the request body against a validation schema using the validateLoginSchema function.
-//* It then checks if a user already exists with the email provided in the request body using the findUserByEmail function.
-//* If the user does not exist, an error message of "invalid email/password" is thrown.
-//* If the user does exist, the plaintext password provided in the request body is compared with the hashed password stored in the user object using the cmpHash function.
-//* If they do not match, an error message of "invalid email/password" is thrown.
-//* If they do match, a token is generated using the genToken function and passed a payload of the user's name, email, id, admin status, and wishlist.
-//* This token is then sent in the response along with a status code of 201.
-//* If any errors occur during this process, the error message is sent in the response instead, with a status code of 400.
+
 router.post("/login", async(req, res) => {
     try {
         const validatedValue = await validateLoginSchema(req.body);
@@ -84,11 +69,6 @@ router.post("/login", async(req, res) => {
 
 
 //* Auto login
-//* This code is a route handler for a POST request to the "/userinfo" endpoint using the Express.js router.
-//* When this route is hit, it extracts the token from the headers of the request and assigns it to the variable "token".
-//* Then it uses a function called verifyToken with the token variable as an argument, this function will verify the token and return the payload associated with the token.
-//* If the token is valid, it will respond with a json object containing the user information that was passed in the payload of the token.
-//* If any error occurs during the process, such as an invalid token, it will respond with an error message and a status code of 400.
 router.post("/userinfo", async(req, res) => {
     try {
         let token = req.headers.token;
@@ -100,29 +80,29 @@ router.post("/userinfo", async(req, res) => {
 });
 
 //* forgot password
-router.post("/forgotpassword", async(req, res) => {
-    try {
-        const validatedValue = await validateForgotPasswordSchema(req.body);
-        const userData = await findUserByEmail(validatedValue.email);
-        if (!userData) throw "check your inbox";
-        const jwt = await genToken({ email: userData.email }, "1h");
-        res.json({ msg: "check your inbox" });
-    } catch (err) {
-        res.json({ msg: err });
-    }
-});
+// router.post("/forgotpassword", async(req, res) => {
+//     try {
+//         const validatedValue = await validateForgotPasswordSchema(req.body);
+//         const userData = await findUserByEmail(validatedValue.email);
+//         if (!userData) throw "check your inbox";
+//         const jwt = await genToken({ email: userData.email }, "1h");
+//         res.json({ msg: "check your inbox" });
+//     } catch (err) {
+//         res.json({ msg: err });
+//     }
+// });
 
 //* reset password
-router.post("/resetpassword/:token", async(req, res) => {
-    try {
-        const userData = await findUserByEmail(payload.email);
-        if (!userData) throw "something went wrong";
-        const hashedPassword = await createHash(req.body.password);
-        await updatePasswordById(userData._id, hashedPassword);
-        res.json({ msg: "password updated" });
-    } catch (err) {
-        res.status(400).json({ err });
-    }
-});
+// router.post("/resetpassword/:token", async(req, res) => {
+//     try {
+//         const userData = await findUserByEmail(payload.email);
+//         if (!userData) throw "something went wrong";
+//         const hashedPassword = await createHash(req.body.password);
+//         await updatePasswordById(userData._id, hashedPassword);
+//         res.json({ msg: "password updated" });
+//     } catch (err) {
+//         res.status(400).json({ err });
+//     }
+// });
 
 module.exports = router;
--- routes/api/gamesCards.js
+++ routes/api/gamesCards.js
@@ -20,10 +20,7 @@ const authMiddleware = require("../../middleware/auth.middleware");
 
 const allowAccessMiddleware = require("../../middleware/allowModify.middleware");
 
-//* This code is a route handler for a GET request to the root endpoint ("/") using the Express.js router.
-//* When this route is hit, it attempts to retrieve all game cards using the showAllGameCards function, which access the database to get all game cards records.
-//* If the operation is successful, it will respond with a JSON object containing all the game cards.
-//* If any error occurs during this process, it will respond with an error message and a status code of 400.
+
 router.get("/", async(req, res) => {
     try {
         const allGamecards = await showAllGameCards();
@@ -33,14 +30,7 @@ router.get("/", async(req, res) => {
     }
 });
 
-//* This code is a route handler for a POST request to the root endpoint ("/") using the Express.js router.
-//* When this route is hit, it first uses an authMiddleware function to check the request for valid authentication before proceeding, it likely checks for a valid token in the headers,
-//* and decoded the token to get user data.
-//* Then it attempts to validate the request body against a validation schema using the validateNewGameSchema function.
-//* After that it creates a new game card using the createNewGameCards function, passing in the validated values from the request body, as well as the user's ID from the decoded token,
-//* this function access the database to create the new game card.
-//* If the operation is successful, it will respond with a JSON object containing the data of the newly created game card and a status code of 201.
-//* If any error occurs during this process, such as a problem with the database or an invalid request, it will respond with an error message and a status code of 400.
+
 router.post("/", authMiddleware, async(req, res) => {
     try {
         const validatedValue = await validateNewGameSchema(req.body);
@@ -59,18 +49,7 @@ router.post("/", authMiddleware, async(req, res) => {
     }
 });
 
-//* This code is a route handler for a PATCH request to a specific endpoint ("/:id") using the Express.js router.
-//* When this route is hit, it first uses an authMiddleware function to check the request for valid authentication before proceeding,
-//* it likely checks for a valid token in the headers, and decoded the token to get user data.
-//* Then it uses the allowAccessMiddleware function to check if the user has the permission to access this route.
-//* After that it attempts to validate the request body against a validation schema using the validateUpdateGameSchema function.
-//* After that, it retrieves the game card with the specified ID from the database using the showGameCardById function.
-//* It then checks if the game card exists, if not it throws an error "game not exists",
-//* else it checks if the user has permission to update the game card using the allowAccess attribute of the userData from the token.
-//* If the user has permission, it updates the game card in the database using the updateGamecardById function and passing in the updated values from the request body,
-//* as well as the game card's ID from the request parameters.
-//* If the operation is successful, it will respond with the updated game card data and a status code of 201.
-//* If any error occurs during this process, such as a problem with the database or an invalid request, it will respond with an error message and a status code of 401.
+
 router.patch(
     "/:id",
     authMiddleware,
@@ -101,11 +80,6 @@ router.patch(
 );
 
 //* delete
-//* A route for deleting a game card by its ID.
-//* This route is protected by an authentication middleware and an access control middleware.
-//* If the user is authenticated and has the appropriate access level, the route will proceed to delete the game card.
-//* The game card ID is passed as a parameter in the route and is used to find and delete the game card from the database.
-//* If the user is not authenticated or does not have the appropriate access level, an error message will be returned.
 router.delete(
     "/:id",
     authMiddleware,
@@ -126,11 +100,6 @@ router.delete(
 );
 
 //*params - getbyid
-//* This route is handling a GET request to the "/getbyid/:id" endpoint.
-//* It is using a "validateFindByIdSchema" function to validate the request parameters, specifically the "id" parameter in the url.
-//* Then it is using a "showGameCardById" function to retrieve the game data associated with the provided id.
-//* If the game data is found, it is returned to the client with a status code of 200.
-//* If there is an error, it is returned to the client with a status code of 400.
 router.get("/getbyid/:id", async(req, res) => {
     try {
         const validatedValue = await validateFindByIdSchema(req.params);
